e4ba8f1ec54b523bbbb73eef26f38b5a3517d2d3
hello0zai <hello0zaizai@gmail.com>
2025-05-19 15:39:42 +0700

    Added not to show "Sundial is already running." while clicking on the desktop icon multiple times.

--- sd_qt/__main__.py
+++ sd_qt/__main__.py
@@ -4,26 +4,27 @@ import tempfile
 import ctypes
 from sd_qt.main import main
 
+main()
 
-def show_error_message():
-    ctypes.windll.user32.MessageBoxW(
-        0, "Sundial is already running.", "Sundial.exe", 0x10)
+# def show_error_message():
+#     ctypes.windll.user32.MessageBoxW(
+#         0, "Sundial is already running.", "Sundial.exe", 0x10)
 
 
-tempdir = tempfile.gettempdir()
-lockfile = os.sep.join([tempdir, 'myapp.lock'])
-try:
-    if os.path.isfile(lockfile):
-        os.unlink(lockfile)
-# Should give you smth like 'WindowsError: [Error 32] The process cannot access the file because it is being used by another process..'
-except WindowsError as e:
-    ctypes.windll.user32.MessageBoxW(0,
-                                     "Sundial is already running.",
-                                     "Sundial.exe",
-                                     0x10)
-    sys.exit(0)
+# tempdir = tempfile.gettempdir()
+# lockfile = os.sep.join([tempdir, 'myapp.lock'])
+# try:
+#     if os.path.isfile(lockfile):
+#         os.unlink(lockfile)
+# # Should give you smth like 'WindowsError: [Error 32] The process cannot access the file because it is being used by another process..'
+# except WindowsError as e:
+#     # ctypes.windll.user32.MessageBoxW(0,
+#     #                                  "Sundial is already running.",
+#     #                                  "Sundial.exe",
+#     #                                  0x10)
+#     sys.exit(0)
 
-with open(lockfile, 'wb') as lockfileobj:
-    # run your app's main here
-    main()
-os.unlink(lockfile)
+# with open(lockfile, 'wb') as lockfileobj:
+#     # run your app's main here
+#     main()
+# os.unlink(lockfile)
