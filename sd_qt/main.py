ebdeab622d590818e5e13888fce6a14df64dd938
chompu-cs <chompu@zktecodev.com>
2025-06-17 15:49:26 +0700

    Added checking if server is up or not.

--- sd_qt/main.py
+++ sd_qt/main.py
@@ -7,6 +7,7 @@ import signal
 import threading
 import requests
 from time import sleep
+from datetime import datetime
 
 from sd_core.log import setup_logging
 from sd_qt.keychain_script import clear_keys
@@ -14,8 +15,26 @@ from sd_qt.manager import Manager
 from .config import AwQtSettings
 from .sd_desktop.main import run_application
 from sd_qt.sd_desktop.monitor import start_exe
+from sd_qt.sd_desktop.util import check_server_status
 
 logger = logging.getLogger(__name__)
+
+
+# Wait for the server to become available
+def wait_until_server_is_up():
+    for _ in range(60):
+        try:
+            logger.info(f"check_server_status() {check_server_status()}")
+            if check_server_status() == False:
+                now = datetime.now()
+                logger.info(f"check_server_status() time {now}")
+                sleep(1)
+            else:
+                return True
+        except:
+            sleep(1)
+            
+    return False
 
 def main() -> None:
     """
@@ -39,10 +58,13 @@ def main() -> None:
 
         clear_keys()
         logger.info("before main starting sd-server")
-        start_exe("sd-server")
+        threading.Thread(target=start_exe, args=("sd-server",), daemon=True).start()
         logger.info("after main starting sd-server")
 
-        run_application()
+        if wait_until_server_is_up():
+            run_application()
+        else:
+            logger.info(f"server not running..... ")
 
         if sys.platform == "win32":
             try:
@@ -52,7 +74,7 @@ def main() -> None:
         else:
             def handle_signal(signum, frame):
                 logger.info(f"Signal {signum} received, stopping...")
-                manager.stop_all()
+                # manager.stop_all()
                 sys.exit(0)
 
             signal.signal(signal.SIGTERM, handle_signal)
